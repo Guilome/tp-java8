76f6e033f1b19004a4fa7c04fba29a77f2e9c6eb
Guillaume <guillaume.gobert@gmail.com>
2018-03-06 17:49:25 +0100

    Function

--- src/test/java/java8/ex06/Function_06_Test.java
+++ src/test/java/java8/ex06/Function_06_Test.java
@@ -21,8 +21,8 @@ public class Function_06_Test {
     // TODO compléter la méthode
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
     String formatAge(Supplier<Person> supplier) {
-        // TODO
-        return null;
+    	String chaine = "";
+        return chaine = "[age=" + supplier.get().getAge() + "]";
     }
     // end::formatAge[]
 
@@ -30,7 +30,7 @@ public class Function_06_Test {
     @Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+        String result = formatAge(()-> new Person("John", "France", 35, "pass"));
 
         assert result.equals("[age=35]");
     }
